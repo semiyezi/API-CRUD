e243d68b7dccfa1388140d0b99ca12b6f0059a4c
semiyezi <tuvulukidi@gmail.com>
2020-07-15 22:31:20 +0100

    crud_avec_knex

---
+++ app_knex.js
@@ -0,0 +1,80 @@
+const express=require("express");
+const mysql=require("mysql");
+const bodyParser=require("body-parser");
+
+
+
+const app=express();
+
+
+const knex = require('knex')({
+    client: 'mysql',
+    connection: {
+      host : 'localhost',
+      user : 'semia',
+      password : 'root',
+      database : 'test1'
+    }
+  });
+  // parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+ 
+// parse application/json
+app.use(bodyParser.json())
+
+//récuperation tout des utilisateurs
+app.get('/api/users',(req,res)=>{
+    knex.select().from('user').then((result)=>{
+       
+         return res.send(result);
+    })
+})
+//récuperation d'un utilisateur
+app.get('/api/users/:id',(req,res)=>{
+    knex.select().from('user').where({ id:req.params.id }).then((result)=>{
+       
+         return res.send(result);
+    })
+})
+
+
+//3.modification d'un utilisateur
+app.put('/api/users/:id',(req,res)=>{
+knex('user').where({id:req.params.id}).update({
+    nom:req.body.nom,
+    postnom:req.body.postnom,
+    age:req.body.age
+  }).then(()=>{
+    return res.send({message:'utilisateur modifié avec succès'});
+})
+})
+
+
+
+//4delete
+app.delete('/api/users/:id',(req,res)=>{
+    knex('user')
+    .where('id', req.params.id)
+    .del().then(()=>{
+      return res.send({message:'utilisateur supprimé avec succès'});
+  })
+  })
+
+  //5 creer un utilisateur
+  app.post('/api/users/',(req,res)=>{
+    knex('user').insert({
+        nom:req.body.nom,
+        postnom:req.body.postnom,
+        age:req.body.age
+      }).then(()=>{
+        return res.send({message:'utilisateur ajouté avec succès'});
+    })
+    })
+    
+
+
+
+const port=5000;
+app.listen(port,function(){
+    console.log(`le serveur écoute sur ce port ${port}`)
+}) 
